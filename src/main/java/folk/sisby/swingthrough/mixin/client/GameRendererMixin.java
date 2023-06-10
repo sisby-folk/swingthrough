d285893ab7aa49f74e7458139e56cac4d08986ef
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-06-10 16:59:27 +1000

    fix up a bit

--- src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java
+++ src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java
@@ -32,13 +32,9 @@ public class GameRendererMixin {
 
 	@ModifyVariable(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/hit/EntityHitResult;getPos()Lnet/minecraft/util/math/Vec3d;"), ordinal = 1)
 	private Entity discardEmptyBlockHit(Entity hitEntity, float tickDelta) {
-		if (this.client.world != null && this.client.player != null && client.crosshairTarget instanceof BlockHitResult bhr && this.client.world.getBlockState(bhr.getBlockPos()).getCollisionShape(this.client.world, bhr.getBlockPos()).isEmpty() && this.client.player.squaredDistanceTo(hitEntity) < getReach() && hitEntity instanceof LivingEntity && !hitEntity.isSpectator() && hitEntity.isAttackable() && !(hitEntity instanceof Tameable het && het.getOwnerUuid() == this.client.player.getUuid()) && !hitEntity.equals(this.client.player.getVehicle())) {
+		if (this.client.world != null && this.client.player != null && client.crosshairTarget instanceof BlockHitResult bhr && this.client.world.getBlockState(bhr.getBlockPos()).getCollisionShape(this.client.world, bhr.getBlockPos()).isEmpty() && this.client.player.squaredDistanceTo(hitEntity) < cachedOriginalReach && hitEntity instanceof LivingEntity && !hitEntity.isSpectator() && hitEntity.isAttackable() && !(hitEntity instanceof Tameable het && het.getOwnerUuid() == this.client.player.getUuid()) && !hitEntity.equals(this.client.player.getVehicle())) {
 			client.crosshairTarget = null;
 		}
 		return hitEntity;
-	}
-
-	private double getReach() {
-		return 9.0;
 	}
 }
