77893e59eaa22d0d6f3f1f964ba91549ffe74c98
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-08-12 14:18:39 +1000

    clean up

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
-<center>Allows targeting living entities through transparent blocks.
+<!--suppress HtmlDeprecatedTag, XmlDeprecatedElement -->
+<center>
+<img alt="preview" src="https://cdn.modrinth.com/data/JyKlunuD/images/e1fb90dd7ba165b9043fbd4b752c42103dc9e7ef.png"/>
+Allows targeting living entities through transparent blocks.
 </center>
 
----
-
-<center><b>Packs:</b> <a href="https://modrinth.com/modpack/tinkerers-quilt">Tinkerer's Quilt</a> (<a href="https://modrinth.com/modpack/tinkerers-silk">Silk</a>) - <a href="https://modrinth.com/modpack/switchy-pack">Switchy Pack</a></center>
-<center><b>Mods:</b> <a href="https://modrinth.com/mod/switchy">Switchy</a> - <a href="https://modrinth.com/mod/origins-minus">Origins Minus</a> (<a href="https://modrinth.com/mod/tinkerers-statures">Statures</a>) - <a href="https://modrinth.com/mod/tinkerers-smithing">Tinkerer's Smithing</a></center>
-
----
-
 This mod discards the block crosshair target to allow an entity to be targeted instead, when:
-   - The block has empty collision
-   - The entity is living and attackable
-   - You're not directly riding the entity
-   - The entity isn't your own tamed entity
-   - The entity is within reach (*slightly* longer than actual attack range, but still usable)
+
+- The block has empty collision
+- The entity is living and attackable
+- You're not directly riding the entity
+- The entity isn't your own tamed entity
+- The entity is within reach (*slightly* longer than actual attack range, but still usable)
 
 Due to the way this is implemented, the block outline will also visually disappear when you're targeting the entity instead.
-
 
 #### Technical Notes
 
 This mod is comprised of a [single mixin](https://github.com/sisby-folk/swingthrough/blob/1.19/src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java) with about 4 lines of meaningful code:
 
- - 2 lines to use the full entity targeting range if the block meets the conditions (instead of stopping at the block).
- - 2 lines to discard the block target if the full range was used and the entity meets the conditions.
+- 2 lines to use the full entity targeting range if the block meets the conditions (instead of stopping at the block).
+- 2 lines to discard the block target if the full range was used and the entity meets the conditions.
 
 It performs no additional raycasts or radius searches.
 
--- src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java
+++ src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java
@@ -7,37 +7,30 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Tameable;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.util.Objects;
-
-@SuppressWarnings("ConstantConditions")
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-	@Shadow @Final private MinecraftClient client;
-	@Unique private Double cachedReach = null;
+	private Double swingthrough$reach = null;
 
 	@ModifyVariable(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D", ordinal = 0), ordinal = 1)
 	private double cacheReach(double original) {
-		cachedReach = client.crosshairTarget.getType() == HitResult.Type.BLOCK && client.crosshairTarget instanceof BlockHitResult bhr && this.client.world.getBlockState(bhr.getBlockPos()).getCollisionShape(this.client.world, bhr.getBlockPos()).isEmpty() ? original : null;
+		swingthrough$reach = MinecraftClient.getInstance().crosshairTarget.getType() == HitResult.Type.BLOCK && MinecraftClient.getInstance().crosshairTarget instanceof BlockHitResult && MinecraftClient.getInstance().world.getBlockState(((BlockHitResult) MinecraftClient.getInstance().crosshairTarget).getBlockPos()).getCollisionShape(MinecraftClient.getInstance().world, ((BlockHitResult) MinecraftClient.getInstance().crosshairTarget).getBlockPos()).isEmpty() ? original : null;
 		return original;
 	}
 
 	@ModifyArg(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileUtil;raycast(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;D)Lnet/minecraft/util/hit/EntityHitResult;"), index = 5)
 	private double useOriginalReachForEntityRaycast(double original) {
-		return Objects.requireNonNullElse(cachedReach, original);
+		return swingthrough$reach == null ? original : swingthrough$reach;
 	}
 
 	@ModifyVariable(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/hit/EntityHitResult;getPos()Lnet/minecraft/util/math/Vec3d;"), ordinal = 1)
 	private Entity discardEmptyBlockHit(Entity hitEntity, float tickDelta) {
-		if (cachedReach != null && this.client.player != null && this.client.getCameraEntity().getCameraPosVec(tickDelta).squaredDistanceTo(hitEntity.getPos()) < cachedReach && hitEntity instanceof LivingEntity && !hitEntity.isSpectator() && hitEntity.isAttackable() && !(hitEntity instanceof Tameable het && het.getOwnerUuid() == this.client.player.getUuid()) && !hitEntity.equals(this.client.player.getVehicle())) {
-			client.crosshairTarget = null;
+		if (swingthrough$reach != null && MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().getCameraEntity().getCameraPosVec(tickDelta).squaredDistanceTo(hitEntity.getPos()) < swingthrough$reach && hitEntity instanceof LivingEntity && !hitEntity.isSpectator() && hitEntity.isAttackable() && !(hitEntity instanceof Tameable && ((Tameable) hitEntity).getOwnerUuid() == MinecraftClient.getInstance().player.getUuid()) && !hitEntity.equals(MinecraftClient.getInstance().player.getVehicle())) {
+			MinecraftClient.getInstance().crosshairTarget = null;
 		}
 		return hitEntity;
 	}
