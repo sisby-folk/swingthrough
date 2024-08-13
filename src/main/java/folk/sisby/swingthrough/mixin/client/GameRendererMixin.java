5beef1e3b3a353d7a6e7a710e26aabdad82d507f
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-08-13 11:46:10 +1000

    start 1.21

--- src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java
+++ src/main/java/folk/sisby/swingthrough/mixin/client/GameRendererMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,22 +16,23 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class GameRendererMixin {
 	private Double swingthrough$reach = null;
 
-	@ModifyVariable(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D", ordinal = 0), ordinal = 1)
+	@ModifyVariable(method = "findCrosshairTarget", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/lang/Math;max(DD)D", ordinal = 0), ordinal = 0)
 	private double cacheReach(double original) {
 		swingthrough$reach = MinecraftClient.getInstance().crosshairTarget.getType() == HitResult.Type.BLOCK && MinecraftClient.getInstance().crosshairTarget instanceof BlockHitResult && MinecraftClient.getInstance().world.getBlockState(((BlockHitResult) MinecraftClient.getInstance().crosshairTarget).getBlockPos()).getCollisionShape(MinecraftClient.getInstance().world, ((BlockHitResult) MinecraftClient.getInstance().crosshairTarget).getBlockPos()).isEmpty() ? original : null;
 		return original;
 	}
 
-	@ModifyArg(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "method_18075", remap = false), index = 5)
+	@ModifyArg(method = "findCrosshairTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileUtil;raycast(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;D)Lnet/minecraft/util/hit/EntityHitResult;", remap = false), index = 5)
 	private double useOriginalReachForEntityRaycast(double original) {
 		return swingthrough$reach == null ? original : swingthrough$reach;
 	}
 
-	@ModifyVariable(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/hit/EntityHitResult;getPos()Lnet/minecraft/util/math/Vec3d;"), ordinal = 1)
-	private Entity discardEmptyBlockHit(Entity hitEntity) {
-		if (swingthrough$reach != null && MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().getCameraEntity().getPos().squaredDistanceTo(hitEntity.getPos()) < swingthrough$reach && hitEntity instanceof LivingEntity && !hitEntity.isSpectator() && hitEntity.isAttackable() && !hitEntity.equals(MinecraftClient.getInstance().player.getVehicle())) {
+	@ModifyVariable(method = "findCrosshairTarget", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/projectile/ProjectileUtil;raycast(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;D)Lnet/minecraft/util/hit/EntityHitResult;"))
+	private EntityHitResult discardEmptyBlockHit(EntityHitResult hitResult) {
+		Entity hitEntity = hitResult == null ? null : hitResult.getEntity();
+		if (hitEntity != null && swingthrough$reach != null && MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().getCameraEntity().getPos().squaredDistanceTo(hitEntity.getPos()) < swingthrough$reach && hitEntity instanceof LivingEntity && !hitEntity.isSpectator() && hitEntity.isAttackable() && !hitEntity.equals(MinecraftClient.getInstance().player.getVehicle())) {
 			MinecraftClient.getInstance().crosshairTarget = null;
 		}
-		return hitEntity;
+		return hitResult;
 	}
 }
